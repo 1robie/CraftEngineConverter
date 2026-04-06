dfa1d5901acbfa3407d246a9ecdc36d5ed6408a4
1robie <97293924+1robie@users.noreply.github.com>
2026-04-06 14:36:28 +0200

    feat: implement UseEffectsConfiguration with BedrockComponent interface and JSON serialization
    Signed-off-by: 1robie <97293924+1robie@users.noreply.github.com>

--- API/src/main/java/fr/robie/craftengineconverter/api/configuration/item/components/UseEffectsConfiguration.java
+++ API/src/main/java/fr/robie/craftengineconverter/api/configuration/item/components/UseEffectsConfiguration.java
@@ -1,11 +1,13 @@
 package fr.robie.craftengineconverter.api.configuration.item.components;
 
+import com.google.gson.JsonObject;
+import fr.robie.craftengineconverter.api.configuration.bedrock.mapping.item.component.BedrockComponent;
 import fr.robie.craftengineconverter.api.configuration.item.ItemConfigurationSerializable;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.jetbrains.annotations.NotNull;
 
-public class UseEffectsConfiguration implements ItemConfigurationSerializable {
+public class UseEffectsConfiguration implements ItemConfigurationSerializable, BedrockComponent {
 
     private final boolean canSprint;
     private final double speedMultiplier;
@@ -15,6 +17,16 @@ public class UseEffectsConfiguration implements ItemConfigurationSerializable {
         this.canSprint = canSprint;
         this.speedMultiplier = speedMultiplier;
         this.interactVibrations = interactVibrations;
+    }
+
+    /**
+     * To use for bedrock items, as they don't have the can_sprint property is not present in bedrock.
+     *
+     * @param speedMultiplier
+     * @param interactVibrations
+     */
+    public UseEffectsConfiguration(double speedMultiplier, boolean interactVibrations) {
+        this(false, speedMultiplier, interactVibrations);
     }
 
     @Override
@@ -33,5 +45,20 @@ public class UseEffectsConfiguration implements ItemConfigurationSerializable {
         if (!this.interactVibrations) {
             useEffectsSection.set("interact_vibrations", false);
         }
+    }
+
+    @Override
+    public void applyTo(@NotNull JsonObject componentObject) {
+        JsonObject useEffectsComponent = new JsonObject();
+
+        if (this.speedMultiplier != 0.2) {
+            useEffectsComponent.addProperty("speed_multiplier", Math.max(0.0, Math.min(1.0, this.speedMultiplier)));
+        }
+
+        if (!this.interactVibrations) {
+            useEffectsComponent.addProperty("interact_vibrations", false);
+        }
+
+        componentObject.add("minecraft:use_effects", useEffectsComponent);
     }
 }
